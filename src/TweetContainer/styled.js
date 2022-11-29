7f3589b416cdbf44bcfe027ee3b4e4fe888ccbfa
Maria <isaeva.maria.200@gmail.com>
2022-11-29 22:28:30 +0100

    Modify design

--- src/TweetContainer/styled.js
+++ src/TweetContainer/styled.js
@@ -3,23 +3,24 @@ import styled from "styled-components";
 export const MainTweetDiv = styled.div`
 height: 15vh; 
 width: 60vw; 
-border: 1px solid black; 
+border: 2px white; 
 margin-top: 3vh;
 border-radius: 16px;
 //PLAY WITH THIS
-background-color: grey; 
-color: white;
+background-color: aliceblue; 
+color: black;
 
 #tweetAuthor{
     font-size: 0.8rem; 
     font-style: italic; 
     padding: 1% 2%; 
+    color: navy;
 }
 
 #tweetContent{
-    font-size: 1rem; 
+    font-size: 1.1rem; 
     font-family: Roboto, sans-serif;
-    padding: 0% 2%; 
+    padding: 0% 6%; 
 }
 
 `
